365acb6e991bb76aa6f937074c3d269e1581debe
ryanpsouzaa <ryanpsouzagit@gmail.com>
2025-02-12 11:20:36 -0300

    implementando spring security - stateless

---
+++ src/main/java/med/voll/api/domain/usuarios/Usuario.java
@@ -0,0 +1,23 @@
+package med.voll.api.domain.usuarios;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+
+    private String senha;
+}
